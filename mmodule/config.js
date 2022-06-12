44f418088f903cb7ab62b873e8c96431724d5fb6
dungnt <nguyendung87neol@hotmail.com>
2022-06-12 11:51:44 +0700

    Upadate

---
+++ mmodule/config.js
@@ -0,0 +1,9 @@
+//const hostname = '127.0.0.1';
+//const port = 3000;
+
+//module.exports.hostName = hostname;
+//module.exports.port = port;
+module.exports = {
+    hostName: '127.0.0.1',
+    port: 3000
+}
